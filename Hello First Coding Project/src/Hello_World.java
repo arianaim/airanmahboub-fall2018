185cdfb9616c7f67a264dd06a7d4e71373242afc
APCS3rd <APCS3rd@MHS-Dreyer-09>
2018-08-20 10:43:35 -0700

    First commit to github

---
+++ Hello First Coding Project/src/Hello_World.java
@@ -0,0 +1,16 @@
+
+/**
+ * Prints message in console
+ * 
+ * @author Ariana Iranmahboub 
+ * @version 8/17/2018
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Ariana Iranmahboub>"); //Display the string
+    }
+}
+
+
